0f4c10f079bacd393a713c0d58379f436de53c9a
Matthew Cornell <mcornell400708@gmail.com>
2023-02-05 23:17:12 -0500

    Updated to use latest inkwell version

--- src/jit.rs
+++ src/jit.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 use llvm_sys::orc2::{*, lljit::*};
 use llvm_sys::error::*;
-use inkwell::LLVMReference;
 use std::ffi::CString;
 use std::rc::Rc;
 pub type JDL = LLVMOrcJITDylibRef;
@@ -20,12 +19,12 @@ impl LLJIT<'_> {
     }
     pub fn add_module(&self, jdl: JDL, module: inkwell::module::Module) {
         unsafe {
-            LLVMConsumeError(LLVMOrcLLJITAddLLVMIRModule(*self.0, jdl, LLVMOrcCreateNewThreadSafeModule(module.get_ref(), LLVMOrcCreateNewThreadSafeContext())));
+            LLVMConsumeError(LLVMOrcLLJITAddLLVMIRModule(*self.0, jdl, LLVMOrcCreateNewThreadSafeModule(module.as_mut_ptr(), LLVMOrcCreateNewThreadSafeContext())));
         }
     }
     pub fn add_object(&self, jdl: JDL, obj: inkwell::memory_buffer::MemoryBuffer) {
         unsafe {
-            LLVMConsumeError(LLVMOrcLLJITAddObjectFile(*self.0, jdl, obj.get_ref()));
+            LLVMConsumeError(LLVMOrcLLJITAddObjectFile(*self.0, jdl, obj.as_mut_ptr()));
         }
     }
     pub fn add_static(&self, jdl: JDL, path: &str) {
@@ -34,7 +33,7 @@ impl LLJIT<'_> {
             LLVMConsumeError(LLVMOrcCreateStaticLibrarySearchGeneratorForPath(
                 &mut gen as *mut _,
                 LLVMOrcLLJITGetObjLinkingLayer(*self.0),
-                path.as_ptr(),
+                path.as_ptr() as *const i8,
                 inkwell::targets::TargetMachine::get_default_triple().as_ptr()
             ));
             LLVMOrcJITDylibAddGenerator(jdl, gen);
@@ -45,7 +44,7 @@ impl LLJIT<'_> {
             let mut gen = std::ptr::null_mut();
             LLVMConsumeError(LLVMOrcCreateDynamicLibrarySearchGeneratorForPath(
                 &mut gen as *mut _,
-                path.as_ptr() ,
+                path.as_ptr() as *const i8,
                 0, None, std::ptr::null_mut()
             ));
             LLVMOrcJITDylibAddGenerator(jdl, gen);
@@ -60,7 +59,7 @@ impl LLJIT<'_> {
         unsafe {
             const NULL: *mut LLVMOrcOpaqueJITDylib = 0 as *mut _;
             let es = LLVMOrcLLJITGetExecutionSession(*self.0);
-            match LLVMOrcExecutionSessionGetJITDylibByName(es, name.as_bytes().as_ptr()) {
+            match LLVMOrcExecutionSessionGetJITDylibByName(es, name.as_bytes().as_ptr() as *const i8) {
                 NULL => None,
                 x => Some(x)
             }
@@ -69,9 +68,9 @@ impl LLJIT<'_> {
     pub fn lookup_or_insert_lib(&self, name: &CString) -> JDL {
         unsafe {
             let es = LLVMOrcLLJITGetExecutionSession(*self.0);
-            let mut jdl = LLVMOrcExecutionSessionGetJITDylibByName(es, name.as_bytes().as_ptr());
+            let mut jdl = LLVMOrcExecutionSessionGetJITDylibByName(es, name.as_bytes().as_ptr() as *const i8);
             if jdl.is_null() {
-                jdl = LLVMOrcExecutionSessionCreateBareJITDylib(es, name.as_bytes().as_ptr());
+                jdl = LLVMOrcExecutionSessionCreateBareJITDylib(es, name.as_bytes().as_ptr() as *const i8);
             }
             jdl
         }
@@ -79,7 +78,7 @@ impl LLJIT<'_> {
     pub fn lookup_main<'jit, T>(&'jit self, name: &CString) -> Option<&'jit T> {
         let mut addr = 0u64;
         unsafe {
-            LLVMConsumeError(LLVMOrcLLJITLookup(*self.0, &mut addr as *mut u64, name.as_bytes().as_ptr()));
+            LLVMConsumeError(LLVMOrcLLJITLookup(*self.0, &mut addr as *mut u64, name.as_bytes().as_ptr() as *const i8));
             (addr as *const T).as_ref()
         }
     }
