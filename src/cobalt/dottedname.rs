29ed55527b84c9b37ff4c23589518a488db6a031
Matthew Cornell <mcornell400708@gmail.com>
2023-02-07 12:42:54 -0500

    Added serialization for CompoundDottedName

--- src/cobalt/dottedname.rs
+++ src/cobalt/dottedname.rs
@@ -1,5 +1,6 @@
 use crate::Location;
 use std::fmt::*;
+use std::io::{self, Read, Write, BufRead};
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct DottedName {
     pub ids: Vec<(String, Location)>,
@@ -30,6 +31,53 @@ pub enum CompoundDottedNameSegment {
     Identifier(String, Location),
     Glob(Location),
     Group(Vec<Vec<CompoundDottedNameSegment>>)
+}
+impl CompoundDottedNameSegment {
+    pub fn save<W: Write>(&self, out: &mut W) -> io::Result<()> {
+        use CompoundDottedNameSegment::*;
+        match self {
+            Identifier(id, _) => {
+                w.write_all(&[1])?;
+                w.write_all(&id.as_bytes())?;
+                w.write_all(&[0])
+            },
+            Glob(_) => w.write_all(&[2]),
+            Group(groups) => {
+                w.write_all(&[3])?;
+                for group in groups.iter() {
+                    group.iter().try_for_each(|id| id.save(out))?;
+                    out.write_all(&[0])?;
+                }
+                w.write_all(&[0])
+            }
+        }
+    }
+    pub fn load<R: Read + BufRead>(buf: &mut R) -> io::Result<Option<Self>> {
+        use CompoundDottedNameSegment::*;
+        let mut c = 0u8;
+        buf.read_exact(std::slice::from_mut(&mut c))?;
+        match c {
+            0 => Ok(None),
+            1 => {
+                let mut name = vec![];
+                buf.read_until(0, &mut name)?;
+                if name.last() == Some(&0) {name.pop();}
+                Ok(Some(Identifier(std::str::from_utf8(&name).expect("Cobalt symbols should be valid UTF-8").to_string(), (0, 0..0))))
+            },
+            2 => Ok(Some(Glob((0, 0..0)))),
+            3 => {
+                let mut out = vec![];
+                loop {
+                    let mut group = vec![];
+                    while let Some(val) = Self::load(buf)? {group.push(val);}
+                    if group.len() == 0 {break}
+                    else {out.push(group);}
+                }
+                Ok(Some(Group(out)))
+            },
+            x => panic!("read CDN segment expecting 0, 1, 2, or 3, got {x}")
+        }
+    }
 }
 impl Display for CompoundDottedNameSegment {
     fn fmt(&self, f: &mut Formatter) -> Result {
@@ -64,7 +112,24 @@ impl CompoundDottedName {
     pub fn absolute(ids: Vec<CompoundDottedNameSegment>) -> Self {Self::new(ids, true)}
     pub fn relative(ids: Vec<CompoundDottedNameSegment>) -> Self {Self::new(ids, false)}
     pub fn local(id: CompoundDottedNameSegment) -> Self {Self::new(vec![id], false)}
-    pub fn matches(name: &DottedName) -> bool {false} // TODO: pattern matching
+    pub fn save<W: Write>(&self, out: &mut W) -> io::Result<()> {
+        out.write_all(&[if self.global {2} else {1}])?;
+        self.ids.iter().try_for_each(|i| i.save(out))?;
+        out.write_all(&[0])
+    }
+    pub fn load<R: Read + BufRead>(buf: &mut R) -> io::Result<Option<Self>> {
+        let mut c = 0u8;
+        buf.read_exact(std::slice::from_mut(&mut c))?;
+        match c {
+            0 => Ok(None),
+            1 | 2 => {
+                let mut ids = vec![];
+                while let Some(id) = CompoundDottedNameSegment::load(buf)? {ids.push(id)}
+                Ok(Some(Self::new(ids, c == 2)))
+            },
+            x => panic!("read CDN expecting 0, 1, or 2, got {x}")
+        }
+    }
 }
 impl From<DottedName> for CompoundDottedName {
     fn from(other: DottedName) -> Self {Self::new(other.ids.into_iter().map(|(id, loc)| CompoundDottedNameSegment::Identifier(id, loc)).collect(), other.global)}
