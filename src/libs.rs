b910713333c38640749556923453e216d296877a
Matthew Cornell <mcornell400708@gmail.com>
2023-02-18 15:52:37 -0500

    Expanded library search to use libs with SONAME

--- src/libs.rs
+++ src/libs.rs
@@ -6,10 +6,7 @@ pub fn find_libs<'a>(mut libs: Vec<String>, dirs: &Vec<&str>, ctx: Option<&cobal
     let mut out = vec![];
     for x in dirs.iter().flat_map(|dir| walkdir::WalkDir::new(dir).follow_links(true).into_iter()).filter_map(|x| x.ok()).filter(|x| x.file_type().is_file()) {
         let path = x.into_path();
-        match path.extension().and_then(OsStr::to_str) {
-            Some("so") | Some("dylib") | Some("dll") => {},
-            _ => continue
-        }
+        if let Some(ext) = path.file_name().and_then(OsStr::to_str).map(|x| x.find('.').map(|i| &x[i..]).unwrap_or(x)) {if !(ext.contains(".so") || ext.contains(".dylib") || ext.contains(".dll")) {continue}} else {continue}
         if let Some(stem) = path.file_stem().and_then(|x| x.to_str()) {
             for lib in libs.iter_mut().filter(|x| x.len() > 0) {
                 if lib == &stem || (stem.starts_with("lib") && lib == &&stem[3..]) {
