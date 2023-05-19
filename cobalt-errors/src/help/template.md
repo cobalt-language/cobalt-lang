0fe6c00f6e01cbdf7a26f00789f5d3b0fc399b0f
Matthew Cornell <mcornell400708@gmail.com>
2023-05-19 13:06:27 -0400

    Change error handling from codespan to miette

--- cobalt-errors/src/help/template.md
+++
@@ -1,8 +0,0 @@
-Something went wrong.
-# Erroneous code
-```
-
-```
-# Help
-- Suggestion 1
-- Suggestion 2
---
+++ cobalt-errors/src/old.rs
@@ -0,0 +1,16 @@
+use miette::SourceSpan;
+/// Dummy diagnostic so stuff still compiled. It will be removed
+#[derive(Clone, Debug, PartialEq, Eq)]
+#[deprecated]
+pub struct Diagnostic(pub u64);
+impl Diagnostic {
+    pub fn code(&self) -> u64 {self.0}
+    pub fn is_err(&self) -> bool {self.0 >= 100}
+    pub fn is_warn(&self) -> bool {self.0 < 100}
+    pub fn error(_loc: SourceSpan, _code: u64, _message: Option<String>) -> Self {panic!("errors have been removed!")}
+    pub fn warning(_loc: SourceSpan, _code: u64, _message: Option<String>) -> Self {panic!("errors have been removed!")}
+    pub fn note(self, _loc: SourceSpan, _message: String) -> Self {self}
+    pub fn info(self, _message: String) -> Self {self}
+    pub fn add_note(&mut self, _loc: SourceSpan, _message: String) -> &mut Self {self}
+    pub fn add_info(&mut self, _message: String) -> &mut Self {self}
+}
