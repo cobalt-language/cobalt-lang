4cc48071cac07b84dd42b5df56abad337bddcde8
Matthew Cornell <mcornell400708@gmail.com>
2024-01-19 17:01:20 -0500

    Rewrite the interner

--- cobalt-utils/src/tests/intern.rs
+++ cobalt-utils/src/tests/intern.rs
@@ -1,10 +1,10 @@
 #[test]
 fn test_int_bool_tuple() {
-    static INTERN: crate::Interner<(i32, bool)> = crate::Interner::new();
+    let intern = crate::Interner::<(i32, bool)>::new();
     {
-        let p1 = INTERN.intern((16, true));
-        let p2 = INTERN.intern((64, false));
-        let p3 = INTERN.intern((16, true));
+        let p1 = intern.intern((16, true));
+        let p2 = intern.intern((64, false));
+        let p3 = intern.intern((16, true));
         assert!(!std::ptr::eq(p1, p2));
         assert!(!std::ptr::eq(p2, p3));
         assert!(std::ptr::eq(p1, p3));
